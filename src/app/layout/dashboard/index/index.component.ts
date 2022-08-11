9136678b723ee299b09f1121d72933a9bff28c3e
Gabriel Njoabozia <mails4gabriel@gmail.com>
2022-08-11 03:49:01 +0100

    implemented dashboard

--- src/app/layout/dashboard/index/index.component.ts
+++ src/app/layout/dashboard/index/index.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit {
+  transactions: Array<any> = [1, 2, 3, 4];
+  investments: Array<any> = [
+    {
+      src: 'assets/img/investment-1.svg',
+      name: 'Palm Oil',
+      expiration: 'Expires: 20/03/2022',
+    },
+    {
+      src: 'assets/img/investment-2.svg',
+      name: 'Palm Oil',
+      expiration: 'Expires: 20/03/2022',
+    },
+    {
+      src: 'assets/img/investment-3.svg',
+      name: 'Palm Oil',
+      expiration: 'Expires: 20/03/2022',
+    },
+    {
+      src: 'assets/img/investment-4.svg',
+      name: 'Palm Oil',
+      expiration: 'Expires: 20/03/2022',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
